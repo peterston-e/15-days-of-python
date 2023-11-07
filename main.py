a9e5ec03bb12c9ac5b4e070ba11102766bc18ab0
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2023-11-07 18:34:18 +0000

    added process coins feature

--- main.py
+++ main.py
@@ -30,16 +30,20 @@ resources = {
     "coffee": 100,
 }
 
-quarters = 0.25
-dimes = 0.10
-nickles = 0.05
-pennies = 0.01
+currency = {
+    "quarters": 0.25,
+    "dimes": 0.10,
+    "nickles": 0.05,
+    "pennies": 0.01,
+}
+
+profit = 0
 
 def check_resources(drink):
     water = MENU[drink]["ingredients"]["water"]
     coffee = MENU[drink]["ingredients"]["coffee"]
     if drink != "espresso":
-       milk = MENU[drink]["ingredients"]["coffee"]
+        milk = MENU[drink]["ingredients"]["coffee"]
     else:
         milk = 0
 
@@ -52,22 +56,29 @@ def check_resources(drink):
     else:
         return True
 
+
 # print(MENU["espresso"]["ingredients"]["water"])
 # print(MENU["espresso"]["cost"])
 
-# prompt user.
+# prompt user for drink.
 choice = input("What would you like? (espresso/latte/cappuccino): ")
 
-# TODO: 4. check resources after being prompted for drink
+# check resources after being prompted for drink
 item = check_resources(choice)
 if item != True:
     print(f"Sorry there is not enough {item}.")
 
+# process coins - "insert coins, quarters etc"
+print("Please insert coins")
+total_dollars = 0
+for coin in currency:
+    num_coin = int(input(f"how many {coin}: "))
+    total_dollars += num_coin * currency[coin]
 
+# print(f"test - {'{0:.2f}'.format(total_dollars)}")
 
-
-# TODO: 5. process coins - "insert coins, quarters etc"
 # TODO: 6. check money, if !enough, refund and exit. if ok add to profit
+profit += total_dollars
 # TODO: 7. calculate change
 # TODO: 8. make coffee - deduct resources, “Here is your latte. Enjoy!”
 # TODO: 2. exit if the user types "off"
